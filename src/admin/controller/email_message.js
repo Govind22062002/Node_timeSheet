27935a0f13e3e0e5301cca7e40434969003275ed
niteshsharmAA <niteshsharma2109@gmail.com>
2023-04-13 14:55:42 +0530

    NODE_TIMESHEET || EMAILMODULE

--- src/admin/controller/email_message.js
+++ src/admin/controller/email_message.js
@@ -1,4 +1,46 @@
+const emailModel = require('../../models/emailModel');
+const nodemailer = require('nodemailer');
 
 exports.emailMessage = async (req,res) => {
-    res.render("email_message")
+    const data = await emailModel.find();
+    res.render("emailmessage",{data})
+}
+
+exports.sendMail = async(req, res) => {
+    try {
+        const {To,subject,body} = req.body;
+        let mailTransporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'niteshsharma.img@gmail.com',
+                pass: 'lozevcyhwjnidozt'
+            }
+        });
+
+        let mailDetails = {
+            from: 'niteshsharma.img@gmail.com',
+            to: To,
+            subject: subject,
+            text: body
+        };
+
+        mailTransporter.sendMail(mailDetails, function(err, data) {
+            if(err) {
+                console.log(err,'Error Occurs');
+            } else {
+                console.log('Email sent successfully');
+            }
+        });
+        const data = new emailModel ({
+            "To":To,
+            "subject":subject,
+            "body":body,
+            "from":"niteshsharma.img@gmail.com"
+        })
+        data.save();
+        res.redirect('back');
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 }
--- src/admin/route/emailMessage.js
+++ src/admin/route/emailMessage.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const {emailmessageCtrl} = require("../controller");
 
 router.get("/email_message", emailmessageCtrl.emailMessage )
+router.post('/mail-sent', emailmessageCtrl.sendMail);
 
 module.exports = router ;
