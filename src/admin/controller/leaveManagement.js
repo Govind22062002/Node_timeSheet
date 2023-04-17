c4d3ddc575d87f43d3ea5eced36d4580535c1ded
niteshsharmAA <niteshsharma2109@gmail.com>
2023-04-17 15:43:10 +0530

    test

--- src/admin/controller/leaveManagement.js
+++ src/admin/controller/leaveManagement.js
@@ -12,7 +12,21 @@ exports.viewLeaveRequest = async (req, res) => {
 
 exports.viewLeaveDatatable = async (req, res) => {
     try {
-        // const data = await 
+        const result = await leaveModel.find();
+        let dataa = [];
+        for(let i of result){
+            dataa.push({
+            emp_id:i.employee_Id,
+            leave_type:i.leaveType,
+            from:i.from,
+            to:i.to,
+            reason:i.reason,
+            });
+        }
+        var data = JSON.stringify({
+            "data": dataa
+        });
+        res.send(data);
     } catch (error) {
         console.log(error);
         throw error
