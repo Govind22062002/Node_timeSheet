77410313db828c7a0ad1cb2bd1523d8653f2dbd0
anjali <anjali.revolo@outlook.com>
2023-04-17 16:28:18 +0530

    user model change

--- src/api/controllerApi/usersCtrlApi.js
+++ src/api/controllerApi/usersCtrlApi.js
@@ -1,48 +1,47 @@
-const { teamMatesModel } = require("../../models")
+const { userModel } = require("../../models")
 
-exports.usersGetCtrl = async (req,res) => {
-   try {
-    const data = await teamMatesModel.find()
-    console.log(data, "data");
-    if(data && data.length !== 0){
+exports.usersGetCtrl = async (req, res) => {
+    try {
+        const data = await userModel.find()
+        console.log(data, "data");
+        if (data && data.length !== 0) {
+            res
+                .status(200)
+                .json({
+                    success: true,
+                    data: data
+                })
+        } else {
+            res
+                .status(400)
+                .json({
+                    success: false,
+                    message: "There is no users data available"
+                })
+        }
+    } catch (error) {
         res
-        .status(200)
-        .json({
-            success : true ,
-            data : data
-        })
-    }else {
-        res
-        .status(400)
-        .json({
-            success : false ,
-            message : "There is no users data available"
-        })
+            .status(400)
+            .json({
+                success: false,
+                message: error
+            })
     }
-   } catch (error) {
-    res
-    .status(400)
-    .json({
-        success : false ,
-        message : error
-    })
-   }
-   
-} 
+
+}
 
 exports.usersPostCtrl = async (req, res) => {
     try {
-        console.log(req.body, "fasdkfh");
-        const user = await teamMatesModel.findOne({ email: req.body.email })
+        const user = await userModel.findOne({ email: req.body.email })
         if (user) {
             res
                 .status(400)
                 .json({
                     success: false,
-                    messages: "email Id is allready exist"
+                    messages: "Email Id is allready exist"
                 })
         } else {
-            const data = await teamMatesModel.create({
+            const data = await userModel.create({
                 name: req.body.name,
                 type: req.body.type,
                 email: req.body.email,
--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,6 @@
 exports.registerModel = require("./register")
 exports.leaveManagementModel = require("./leaveManagement")
-exports.teamMatesModel = require("./teamMates")
+exports.userModel = require("./userModel")
 exports.publicHolidayModel = require("./publicHoliday")
 exports.statusManagementModel = require("./statusManagement")
 exports.taskAssignModel = require("./taskAssign")
