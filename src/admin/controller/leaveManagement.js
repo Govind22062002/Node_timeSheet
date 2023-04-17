04c54ddda084780cc686c4a8b37f19af0d2b07ba
niteshsharmAA <niteshsharma2109@gmail.com>
2023-04-17 11:24:35 +0530

    Public folder

--- src/admin/controller/leaveManagement.js
+++ src/admin/controller/leaveManagement.js
@@ -1,3 +1,20 @@
+const leaveModel = require("../../models/leaveManagement");
+
 exports.viewLeaveRequest = async (req, res) => {
-    res.render("viewLeaveRequest")
+    try {
+        res.render("viewLeaveRequest");
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+    
+}
+
+exports.viewLeaveDatatable = async (req, res) => {
+    try {
+        // const data = await 
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
 }
