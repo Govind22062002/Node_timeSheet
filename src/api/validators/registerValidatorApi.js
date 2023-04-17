3e2d5be5d5f9e12ef14e5882a165f977fe88b5c9
Govind singh <govind123.img@gmail.com>
2023-04-17 09:46:55 +0530

    modified Node TimeSheet

--- src/api/validators/registerValidatorApi.js
+++ src/api/validators/registerValidatorApi.js
@@ -1,13 +1,7 @@
 const Joi = require("joi");
 
 const Schema = Joi.object({
-    name: Joi.string()
-        .alphanum()
-        .min(3)
-        .max(30)
-        .required(),
-    email: Joi.string()
-        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).lowercase(),
-    password: Joi.string()
-        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.email().required,
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
 })
