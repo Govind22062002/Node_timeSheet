7613d7ef485774e42001da8dd5135a8b9ee58444
Govind singh <govind123.img@gmail.com>
2023-04-21 13:56:37 +0530

    update Client

--- src/admin/route/clientAssign.js
+++ src/admin/route/clientAssign.js
@@ -6,6 +6,7 @@ const { clientAssignCtrl } = require("../controller");
 
 router.get("/clientAssign",isAuth, clientAssignCtrl.clientAssign);
 router.post("/clientAssignPost",isAuth, clientAssignCtrl.clientAssignPost);
+router.get("/clientUpdate",isAuth, clientAssignCtrl.clientUpdate);
 router.get("/clientDelete/:id",isAuth, clientAssignCtrl.clientDelete);
 
 module.exports = router;
